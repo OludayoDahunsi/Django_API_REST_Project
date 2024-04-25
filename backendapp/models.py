deb2d60ba462c57f739a1c499e3b76af8c5d88d3
Oludayo Dahunsi <Oludayo.Dahunsi@gmail.com>
2024-04-25 01:33:30 -0500

    urlss

--- backendapp/models.py
+++ backendapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class address(models.Model):
+    Address_ID =  models.CharField(max_length=10)
+    Street_Address =models.CharField(max_length=100)
+    City = models.CharField(max_length=100)
+    State = models.CharField(max_length=100)
+    Zip_Code = models.CharField(max_length=100) 
+    Country = models.CharField(max_length=100)
