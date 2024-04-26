caf4493bb419408ffcd17c2d2e5a08fd71ad60ac
Oludayo Dahunsi <Oludayo.Dahunsi@gmail.com>
2024-04-25 21:40:08 -0500

    models and admin added

--- backendapp/admin.py
+++ backendapp/admin.py
@@ -4,5 +4,18 @@ from .models import Person
 
 # Register your models here.
 
-admin.site.register(address)
+
 admin.site.register(Person)
+admin.site.register(Address)
+admin.site.register(Person_Historical_Diagnostic)
+admin.site.register(Organization_Address)
+admin.site.register(Diagnostic)
+admin.site.register(Family)
+admin.site.register(Person_Organization)
+admin.site.register(Person_Phone)
+admin.site.register(Address_Type)
+admin.site.register(Person_Address)
+admin.site.register(Clinic)
+admin.site.register(Clinic_Staff)
+
+
--- backendapp/models.py
+++ backendapp/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
 
-class address(models.Model):
-    Address_ID =  models.CharField(max_length=10)
-    Street_Address =models.CharField(max_length=100)
-    City = models.CharField(max_length=100)
-    State = models.CharField(max_length=100)
-    Zip_Code = models.CharField(max_length=100) 
-    Country = models.CharField(max_length=100)
-
-
-
- 
+# MSSQL Database Created models 
 
 class Person(models.Model):
     GENDER_CHOICES = [
@@ -37,3 +27,82 @@ class Person(models.Model):
     Genogram_Image = models.CharField(max_length=100, blank=True)
     Ecomap_Image = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=255, blank=True)
+
+
+
+class Address(models.Model):
+    Address_ID = models.AutoField(primary_key=True)
+    Street_Address = models.CharField(max_length=100)
+    City = models.CharField(max_length=200)
+    State = models.CharField(max_length=200)
+    Zip_Code = models.CharField(max_length=100)
+    Country = models.CharField(max_length=200)
+
+
+class Person_Historical_Diagnostic(models.Model):
+    Dcode = models.IntegerField()
+    Person_ID = models.IntegerField()
+    Diagnosed_Date = models.CharField(max_length=100)
+    Recovery_Status = models.CharField(max_length=100)
+
+
+class Organization_Address(models.Model):
+    Type = models.CharField(max_length=100)
+    organization_id = models.IntegerField()
+    Address_ID = models.IntegerField()
+
+
+class Diagnostic(models.Model):
+    DCode = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+
+
+
+class Family(models.Model):
+    Family_ID = models.AutoField(primary_key=True)
+    Family_Name = models.CharField(max_length=50)
+
+
+
+class Person_Organization(models.Model):
+    organization_id = models.IntegerField()
+    Relationship_Type = models.CharField(max_length=100)
+    Relationship_Status = models.CharField(max_length=100)
+    Person_ID = models.IntegerField()
+
+
+class Person_Phone(models.Model):
+    Phone_Number = models.CharField(max_length=100)
+    Type = models.CharField(max_length=100)
+    Person_ID = models.IntegerField()
+
+
+
+# mySQL Database Created models 
+
+class Address_Type(models.Model):
+    address_type_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=45)
+
+
+class Person_Address(models.Model):
+    Address_ID = models.IntegerField()
+    E_Mail = models.CharField(max_length=100, help_text='TODO: remove this column in favor of person.email')
+    address_type_id = models.IntegerField()
+    Person_ID = models.IntegerField()
+
+
+class Clinic(models.Model):
+    clinicid = models.AutoField(primary_key=True)
+    Name = models.CharField(max_length=100)
+
+
+
+class Clinic_Staff(models.Model):
+    clinicid = models.IntegerField()
+    Emp_ID = models.IntegerField()
+
+
+
+
+
